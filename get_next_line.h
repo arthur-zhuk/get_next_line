3f9696d05b8bde7f3715e92069caf31fba76028d
Arthur Zhuk <arthurzhuk@gmail.com>
2016-10-20 20:01:06 -0700

    not much progress

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,15 @@
 /*   By: azhuk <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/20 12:59:40 by azhuk             #+#    #+#             */
-/*   Updated: 2016/10/20 13:35:10 by azhuk            ###   ########.fr       */
+/*   Updated: 2016/10/20 17:35:59 by azhuk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <unistd.h>
 
 #define BUFF_SIZE 32
 
--- test_gnl.c
+++ test_gnl.c
@@ -1,28 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   test_gnl.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: azhuk <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/10/20 12:59:27 by azhuk             #+#    #+#             */
-/*   Updated: 2016/10/20 19:58:18 by azhuk            ###   ########.fr       */
+/*   Created: 2016/10/20 14:25:17 by azhuk             #+#    #+#             */
+/*   Updated: 2016/10/20 19:59:28 by azhuk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/get_next_line.h"
-#include <unistd.h>
+#include "libft/libft.h"
+
 #include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 
-int		get_next_line(const int fd, char **line)
+int		main(int argc, char **argv)
 {
-	int		ret;
-	char	*buf;
+	int		fd;
+	int		status;
+	char	*line;
 
-	buf = (char *)malloc(sizeof(char) * BUFF_SIZE + 1);
-	ret = read(fd, buf, BUFF_SIZE);
-	buf[ret] = '\0';
-	line = NULL;
-	return (1);
+	if (argc == 1)
+		fd = 0;
+	else if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		return (2);
+	/*while ((status = get_next_line(fd, &line)) == 1)*/
+	/*{*/
+		/*ft_putendl(line);*/
+		/*free(line);*/
+	/*}*/
+	ft_putnbr(status);
+	if (argc == 2)
+		close(fd);
+	return (0);
 }
