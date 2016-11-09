040572a647b52527ac33d646dc01c79f67faf061
Arthur Zhuk <arthurzhuk@gmail.com>
2016-11-09 13:33:01 -0800

    value change

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: azhuk <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/20 12:59:40 by azhuk             #+#    #+#             */
-/*   Updated: 2016/11/09 10:01:57 by azhuk            ###   ########.fr       */
+/*   Updated: 2016/11/09 13:21:24 by azhuk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 8
+# define BUFF_SIZE 4
 # define EOL '\n'
 
 # include "./libft/libft.h"
