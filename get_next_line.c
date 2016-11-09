708b449e8d371becca3443bf28de78f70583ba0b
Arthur Zhuk <arthurzhuk@gmail.com>
2016-11-09 11:32:23 -0800

    should be all the files

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,114 @@
 /*   By: azhuk <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/20 12:59:27 by azhuk             #+#    #+#             */
-/*   Updated: 2016/10/20 19:58:18 by azhuk            ###   ########.fr       */
+/*   Updated: 2016/11/09 10:38:30 by azhuk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/get_next_line.h"
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
+#include "get_next_line.h"
 
-int		get_next_line(const int fd, char **line)
+t_line	*ilist(const int fd)
+{
+	t_line	*new;
+
+	if (!(new = (t_line *)malloc(sizeof(t_line))))
+		return (NULL);
+	new->fd = fd;
+	new->line = ft_strnew(0);
+	new->next = NULL;
+	return (new);
+}
+
+void	aelem(t_line *list, t_line *new)
+{
+	while (list)
+	{
+		if (list->next == NULL)
+		{
+			list->next = new;
+			new->next = NULL;
+		}
+		list = list->next;
+	}
+}
+
+int		rfile(int fd, t_line *list)
 {
 	int		ret;
-	char	*buf;
+	char	buf[BUFF_SIZE + 1];
+	char	*tmp;
 
-	buf = (char *)malloc(sizeof(char) * BUFF_SIZE + 1);
-	ret = read(fd, buf, BUFF_SIZE);
-	buf[ret] = '\0';
-	line = NULL;
-	return (1);
+	tmp = NULL;
+	ret = -3;
+	while (!ft_strchr(list->line, EOL))
+	{
+		if ((ret = read(fd, buf, BUFF_SIZE)) < 0)
+			return (-1);
+		else
+		{
+			buf[ret] = 0;
+			tmp = list->line;
+			if (!(list->line = ft_strjoin(list->line, buf)))
+				return (-1);
+			free(tmp);
+		}
+		if (ret < BUFF_SIZE)
+			return (ret);
+	}
+	return (ret);
+}
+
+char	*gline(t_line *list, char **line)
+{
+	char	*tmp;
+	char	*text;
+	int		i;
+
+	tmp = NULL;
+	i = 0;
+	text = list->line;
+	while (text[i])
+	{
+		if (text[i] == EOL)
+		{
+			*line = ft_strsub(text, 0, i);
+			tmp = text;
+			text = ft_strdup(text + (i + 1));
+			free(tmp);
+			return (text);
+		}
+		i++;
+	}
+	*line = ft_strdup(text);
+	ft_strclr(text);
+	ft_strclr(list->line);
+	return (text);
+}
+
+int		get_next_line(int const fd, char **line)
+{
+	static t_line	*file;
+	int				ret;
+	t_line			*tmp;
+
+	if (!file)
+		file = ilist(fd);
+	tmp = file;
+	if (fd < 0 || !line)
+		return (-1);
+	while (tmp)
+	{
+		if (tmp->fd == fd)
+			break ;
+		if (tmp->next == NULL)
+			aelem(tmp, ilist(fd));
+		tmp = tmp->next;
+	}
+	if ((ret = rfile(fd, tmp)) == -1)
+		return (-1);
+	tmp->line = gline(tmp, line);
+	if (!ft_strlen(tmp->line) && !ft_strlen(*line) && !ret)
+		return (0);
+	else
+		return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,27 @@
 /*   By: azhuk <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/20 12:59:40 by azhuk             #+#    #+#             */
-/*   Updated: 2016/10/20 17:35:59 by azhuk            ###   ########.fr       */
+/*   Updated: 2016/11/09 10:01:57 by azhuk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# define BUFF_SIZE 8
+# define EOL '\n'
+
+# include "./libft/libft.h"
 # include <fcntl.h>
-# include <unistd.h>
+# include <stdlib.h>
 
-#define BUFF_SIZE 32
+typedef struct			s_line
+{
+	int					fd;
+	char				*line;
+	struct s_line		*next;
+}						t_line;
 
-int		get_next_line(const int fd, char **line);
+int						get_next_line(const int fd, char **line);
 
 #endif
